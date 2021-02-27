3987fa5ee05347eecc56bea3b581e9c3ac56c1c3
hammad-gujar <hammad.gujar36@gmail.com>
2021-02-27 15:54:51 +0100

    Initial Commmit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDaC65yUqPTpgpUSZTDmfIbSQAWi9yuH2E",
+    authDomain: "kanban-fire-10daa.firebaseapp.com",
+    projectId: "kanban-fire-10daa",
+    storageBucket: "kanban-fire-10daa.appspot.com",
+    messagingSenderId: "905509249087",
+    appId: "1:905509249087:web:a8306002c7eb70b35b588a",
+    measurementId: "G-7E4CKM5W3C"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyDaC65yUqPTpgpUSZTDmfIbSQAWi9yuH2E",
+    authDomain: "kanban-fire-10daa.firebaseapp.com",
+    projectId: "kanban-fire-10daa",
+    storageBucket: "kanban-fire-10daa.appspot.com",
+    messagingSenderId: "905509249087",
+    appId: "1:905509249087:web:a8306002c7eb70b35b588a",
+    measurementId: "G-7E4CKM5W3C"
+  }
 };
 
 /*
